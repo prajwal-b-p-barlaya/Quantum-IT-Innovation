1af24f96d5276e35b1d4377709b93356e4990547
Prajwal Barlaya <prajwalbpbarlaya@gmail.com>
2024-02-28 14:54:49 +0530

    first commit

--- clients/src/Pages/Questions/QuestionsDetails.jsx
+++ clients/src/Pages/Questions/QuestionsDetails.jsx
@@ -82,7 +82,7 @@ const QuestionsDetails = () => {
   const dispatch=useDispatch()
 const User= useSelector((state)=>(state.currentUserReducer))
 const location=useLocation()
-const url='http://localhost:3000'
+const url='https://stack-overflow-prajwal-2.onrender.com'
 
 const handlePostAns=(e,answerLength)=>{
     e.preventDefault()
